381a92ebedca853fe6237605c1d22fc22046fb2c
ShnaiderPavel <shnaiderpasha@gmail.com>
2015-11-10 14:44:54 +0300

    первый коммит

---
+++ backends/HuskyToothbrush/src/HuskyToothbrush.cpp
@@ -0,0 +1,18 @@
+/*
+ * HuskyToothbrush.cpp
+ *
+ *  Created on: Nov 10, 2015
+ *      Author: pavel
+ */
+
+#include "HuskyToothbrush.h"
+
+HuskyToothbrush::HuskyToothbrush() {
+    // TODO Auto-generated constructor stub
+
+}
+
+HuskyToothbrush::~HuskyToothbrush() {
+    // TODO Auto-generated destructor stub
+}
+
---
+++ backends/HuskyToothbrush/src/HuskyToothbrush.h
@@ -0,0 +1,17 @@
+/*
+ * HuskyToothbrush.h
+ *
+ *  Created on: Nov 10, 2015
+ *      Author: pavel
+ */
+
+#ifndef BACKENDS_HUSKYTOOTHBRUSH_SRC_HUSKYTOOTHBRUSH_H_
+#define BACKENDS_HUSKYTOOTHBRUSH_SRC_HUSKYTOOTHBRUSH_H_
+
+class HuskyToothbrush {
+public:
+    HuskyToothbrush();
+    virtual ~HuskyToothbrush();
+};
+
+#endif /* BACKENDS_HUSKYTOOTHBRUSH_SRC_HUSKYTOOTHBRUSH_H_ */
